06df0e0ddeddcd26031ec72582f13ee141787fcd
Noé Henchoz <henchoznoe@gmail.com>
2025-10-17 21:24:24 +0200

    Refactor Swagger doc to avoid using JSDoc

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,23 +1,13 @@
+import { OpenApiGeneratorV3 } from '@asteasolutions/zod-to-openapi'
+import registry from '@config/openapi.js'
 import type { Application } from 'express'
-import swaggerJSDoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
 
 export const setupSwagger = (app: Application) => {
-    app.use(
-        '/api-docs',
-        swaggerUi.serve,
-        swaggerUi.setup(
-            swaggerJSDoc({
-                apis: ['src/routes/**/*.ts', 'dist/routes/**/*.js'],
-                definition: {
-                    info: {
-                        description: 'Basic Express Template with TypeScript',
-                        title: 'Express Template',
-                        version: '1.0.0',
-                    },
-                    openapi: '3.0.0',
-                },
-            }),
-        ),
-    )
+    const generator = new OpenApiGeneratorV3(registry.definitions)
+    const openApiDoc = generator.generateDocument({
+        info: { description: 'Basic Express Template', title: 'Express TypeScript API', version: '1.0.0' },
+        openapi: '3.0.0',
+    })
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDoc))
 }
