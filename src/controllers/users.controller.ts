299a375d689651f9b451705ef4b7ab8d69ed3423
Noé Henchoz <henchoznoe@gmail.com>
2025-11-11 11:47:46 +0100

    Refactor health check route to add memory, node, env and app version

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,31 +1,31 @@
 import { AppError } from '@my-types/errors/AppError.js'
 import * as usersService from '@services/users.service.js'
 import { sendSuccess } from '@utils/http-responses.js'
-import type { NextFunction, Request, Response } from 'express'
+import type { Request, Response } from 'express'
 
-export const getAllUsers = async (_: Request, res: Response, next: NextFunction) => {
+export const getAllUsers = async (_: Request, res: Response) => {
     const users = await usersService.getAllUsers()
     if (!users || users.length === 0) throw new AppError('No users found', 404)
     sendSuccess(res, 200, 'Users retrieved successfully', users)
 }
 
-export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+export const getUserById = async (req: Request, res: Response) => {
     const userId = req.params.id
     const user = await usersService.getUserById(userId)
     sendSuccess(res, 200, 'User retrieved successfully', user)
 }
 
-export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+export const createUser = async (req: Request, res: Response) => {
     const newUser = await usersService.createUser(req.body)
     sendSuccess(res, 201, 'User created successfully', newUser)
 }
 
-export const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+export const updateUser = async (req: Request, res: Response) => {
     const updatedUser = await usersService.updateUser(req.body)
     sendSuccess(res, 200, 'User updated successfully', updatedUser)
 }
 
-export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+export const deleteUser = async (req: Request, res: Response) => {
     const userId = req.params.id
     const deletedUser = await usersService.deleteUser(userId)
     sendSuccess(res, 200, 'User deleted successfully', deletedUser)
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,22 @@
+import config from '@config/env.js'
 import { errorLoggerMiddleware } from '@config/logger.js'
 import usersRouter from '@routes/users.route.js'
 import { sendSuccess } from '@utils/http-responses.js'
 import type { Application, Request, Response } from 'express'
+import pkg from '../../package.json' with { type: 'json' }
 
 export const setupRoutes = (app: Application): void => {
     // Health check route
     app.get('/', (_: Request, res: Response) => {
-        sendSuccess(res, 200, 'API is running!', {
+        const healthCheckData = {
+            environment: config.nodeEnv,
+            memory: `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`,
+            node: process.version,
             timestamp: new Date().toLocaleString(),
             uptime: `${process.uptime().toFixed(0)} seconds`,
-        })
+            version: pkg.version,
+        }
+        sendSuccess(res, 200, 'Health check successful', healthCheckData)
     })
 
     // API routes
